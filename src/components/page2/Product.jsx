2343a874ba1d2f9727d4ab72ddc40dc31dd8547a
Abhishek Kumar <imakumarabhishek@gmail.com>
2024-01-24 18:39:44 +0530

    added section 4 of prod page

--- src/components/page2/Product.jsx
+++ src/components/page2/Product.jsx
@@ -3,6 +3,7 @@ import Breadcrump from "./Breadcrump";
 import Section1_p from "./Section1_p";
 import Section2_p from "./Section2_p";
 import Section3_P from "./Section3_P";
+import Section4_P from "./Section4_P";
 
 export default function product() {
   return (
@@ -11,6 +12,7 @@ export default function product() {
       <Section1_p />
       <Section2_p />
       <Section3_P />
+      <Section4_P />
     </div>
   );
 }
