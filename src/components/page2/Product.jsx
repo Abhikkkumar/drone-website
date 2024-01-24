54429340323f0e5d27b3120218e3e40113ab44b3
Abhishek Kumar <imakumarabhishek@gmail.com>
2024-01-24 16:13:21 +0530

    added section 2 of prod

--- src/components/page2/Product.jsx
+++ src/components/page2/Product.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Breadcrump from "./Breadcrump";
 import Section1_p from './Section1_p';
+import Section2_p from './Section2_p';
 
 export default function product() {
   return (
     <div className='product'>
       <Breadcrump />
       <Section1_p />
+      <Section2_p />
+
     </div>
   )
 }
---
+++ src/components/page2/Section2_p.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "../../style/page2/section2_p.css";
+
+
+export default function Section2_p() {
+  return (
+    <div className="section2-prod">
+      <div className="sec2-prod-left">
+        <h1>We Ensure Best Service</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore fuga
+          optio, quia hic qui delectus repellat repudiandae unde deleniti
+          tenetur exercitationem, accusantium iure laborum ducimus quam
+          perferendis a provident in! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Tenetur aspernatur nobis repellendus porro accusamus
+          perferendis sequi, veniam saepe?
+        </p>
+        <ul>
+          <li>
+            <i class="fa-solid fa-check"></i>Lorem ipsum dolor sit amet
+            consectetur adipisicing elit.
+          </li>
+          <li>
+            <i class="fa-solid fa-check"></i>Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Lorem, ipsum dolor.
+          </li>
+          <li>
+            <i class="fa-solid fa-check"></i>Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Lorem ipsum dolor sit.
+          </li>
+          <li>
+            <i class="fa-solid fa-check"></i>Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Lorem, ipsum.
+          </li>
+          <li>
+            <i class="fa-solid fa-check"></i>Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Lorem.
+          </li>
+        </ul>
+      </div>
+      <div className="sec2-prod-right">
+        <div className="sec2-prod-right-inner">
+          <span className="tele-icon">
+            <i class="fa-solid fa-phone"></i>
+          </span>
+          <div>
+            <p>Have you any Questions?</p>
+            <p>
+              Call us <span className="ylo">Today!</span>
+            </p>
+          </div>
+          <div className="dotted-line"></div>
+          <p>(123) 222-8888</p>
+        </div>
+      </div>
+    </div>
+  );
+}
