604ce81c72503efb3024d41b3dd6fcf7e2be738e
Abhishek Kumar <imakumarabhishek@gmail.com>
2024-01-25 16:12:22 +0530

    updated section 2

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../style/section1.css";
 import secImg1 from "../img/section1/vama-ab.png";
 import secImg2 from "../img/section1/about3.png";
+import { Link } from "react-router-dom";
 
 export default function Section1() {
   return (
@@ -20,31 +21,42 @@ export default function Section1() {
         </h6>
         <h2>Vama Skylight</h2>
         <p>
-        Vama Skylight is a leading manufacturer of drones, providing innovative solutions to the drone industry. Our company is dedicated to leveraging the latest technology to help improve operations , reduce costs etc. With a team of experts in the field of unmanned aerial systems, we are committed to developing.
+          Vama Skylight is a leading manufacturer of drones, providing
+          innovative solutions to the drone industry. Our company is dedicated
+          to leveraging the latest technology to help improve operations ,
+          reduce costs etc. With a team of experts in the field of unmanned
+          aerial systems, we are committed to developing.
         </p>
 
         <div className="section-left-middle">
-          <img className="img-2-sec-1" src={secImg2} alt=""/>
+          <img className="img-2-sec-1" src={secImg2} alt="" />
+
           <div className="section-left-left">
             <h5>
               {" "}
-              <i class="fa-regular fa-circle-check"></i>Thea Agriculture Drone 16 Litres 
+              <i class="fa-regular fa-circle-check"></i>Thea Agriculture Drone
+              16 Litres
             </h5>
             <h5>
               {" "}
-              <i class="fa-regular fa-circle-check"></i> Thea Agricultural Drone 10 Litres 
+              <i class="fa-regular fa-circle-check"></i> Thea Agricultural Drone
+              10 Litres
             </h5>
             <h5>
               {" "}
-              <i class="fa-regular fa-circle-check"></i> Naina Disaster Management Drone
+              <i class="fa-regular fa-circle-check"></i> Naina Disaster
+              Management Drone
             </h5>
             <h5>
               {" "}
-              <i class="fa-regular fa-circle-check"></i>    Surveillance Drone
+              <i class="fa-regular fa-circle-check"></i> Surveillance Drone
             </h5>
           </div>
         </div>
-        <button className="know-more">KNOW MORE</button>
+        <Link to="/about">
+          {" "}
+          <button className="know-more">KNOW MORE</button>
+        </Link>
       </div>
     </div>
   );
--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "../style/section2.css";
-import lock from "../img/section2/lock.png";
-import diamond from "../img/section2/diamond.png";
-import medal from "../img/section2/medal.png";
+import Agriculture from "../img/section2/Agriculture-spraying.png";
+import Land from "../img/section2/Land-Mapping.png";
+import Pilot from "../img/section2/Pilot-training.png";
 
 export default function Section2() {
   return (
@@ -12,17 +12,20 @@ export default function Section2() {
           <div className="qlt">
             <div>
               <div className="line l-left"></div>
-              <p>About Quality </p>
+              <p>Services </p>
               <div className="line l-right"></div>
             </div>
 
-            <h2>We're about Quality and Trust</h2>
+            <h2>VAMA Skylight Support & Services</h2>
           </div>
         </div>
       </div>
       <div className="s2-bottom">
         <div className="s2-list">
-          <img src={lock} alt="lock-img" />
+          <div className="s2-list-icons">
+            <img src={Agriculture} alt="lock-img" />
+          </div>
+
           <h2>How to Use Battery</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
@@ -36,7 +39,9 @@ export default function Section2() {
           </div>
         </div>
         <div className="s2-list">
-          <img src={medal} alt="medal" />
+          <div className="s2-list-icons">
+            <img src={Land} alt="medal" />
+          </div>
           <h2>How to Use Controller</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
@@ -49,7 +54,10 @@ export default function Section2() {
           </div>
         </div>
         <div className="s2-list">
-          <img src={diamond} alt="diamond-img" />
+          <div className="s2-list-icons">
+            <img src={Pilot} alt="diamond-img" />
+          </div>
+
           <h2>How to Fly Drone</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
