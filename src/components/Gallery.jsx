b015c20e73e6087d9949d11454646855e55d96af
Abhay <abhaykr15102000@gmail.com>
2024-01-27 18:24:31 +0530

    updated gallery page

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,25 +1,24 @@
-import WSPGallery from './WSPGallery';
-import {galleryImages} from './Template/Data'
-
+import WSPGallery from "./WSPGallery";
+import { galleryImages } from "./Template/Data";
+import BreadcrumpTemp from "./Template/BreadcrumpTemp";
+import "../style/gallery.css";
 
 function Gallery() {
-
- 
+  const breadProp = {
+    title: "Gallery",
+    text: "Our Photos",
+    arr: ["Home", "Gallery"],
+  };
 
   return (
     <div className="Gallery">
-      <br />
-      <div>
+      <BreadcrumpTemp bprops={breadProp} />
+      
+      <div className="gallery-top">
         <h1>Our Photo Gallery</h1>
       </div>
-      <br /><br />
 
-      <WSPGallery
-        galleryImages={galleryImages}
-      />
-
-      <br /><br />
-      
+      <WSPGallery galleryImages={galleryImages} />
     </div>
   );
 }
